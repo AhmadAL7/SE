61f0328ebeddeb99814e6c81861bed10212583e6
MurtazaAliCS <Murtaza.Ali11@outlook.com>
2025-04-21 19:14:43 +0100

    Made manager requests interactive (approve/deny) + updated request page design

--- app/routes/manager_routes.py
+++ app/routes/manager_routes.py
@@ -1,10 +1,19 @@
-# app/routes/manager_routes.py
-from flask import render_template, Blueprint
+from flask import Blueprint, render_template, redirect, url_for, request
 from app.logic.manager_requests_logic import ManagerRequestLogic
 
 manager_bp = Blueprint('manager', __name__)
 
-@manager_bp.route('/requests/manager')
+@manager_bp.route('/requests')
 def requests_manager():
     requests = ManagerRequestLogic.get_all_requests()
     return render_template('requests_manager.html', requests=requests)
+
+@manager_bp.route('/requests/approve/<int:request_id>', methods=['POST'])
+def approve_request(request_id):
+    success, message = ManagerRequestLogic.update_request_status(request_id, "Approved")
+    return redirect(url_for('manager.requests_manager'))
+
+@manager_bp.route('/requests/deny/<int:request_id>', methods=['POST'])
+def deny_request(request_id):
+    success, message = ManagerRequestLogic.update_request_status(request_id, "Denied")
+    return redirect(url_for('manager.requests_manager'))
